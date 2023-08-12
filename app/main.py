4928147152d96038534c71bcdbb2905457d24375
hasanalay <hsn_aly@hotmail.com>
2023-08-12 13:08:37 +0300

    switch to the code first arch. and use orm

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 import time
-from fastapi import Body, FastAPI, Response, status, HTTPException
+from fastapi import Body, Depends, FastAPI, Response, status, HTTPException
 from httpx import post
-from pydantic import BaseModel
-from random import randrange
+from pydantic import BaseModel 
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from sqlalchemy.orm import Session
+from .database import engine, get_db
+from . import models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 # region Models
 class Post(BaseModel):
     Title: str
@@ -17,7 +22,7 @@ class Post(BaseModel):
 # Connection to PostgreSQL DB
 while True:
     try:
-        connection = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='1234', cursor_factory=RealDictCursor)
+        connection = psycopg2.connect(host='localhost', database='fastapi', user='hasanalay', password='1973', cursor_factory=RealDictCursor)
         cursor = connection.cursor()
         print("Connection to PostgreSQL DB successful")
         break
@@ -25,63 +30,50 @@ while True:
         print(f"The error '{e}' occurred")
         time.sleep(2)
 # endregion
-# I am Going to delete this part.
-my_posts = [{"Title": "Hello", "Content": "This is the content of the post.", "Published": True, "Rating": 5, "Id": 1},
-            {"Title": "Book", "Content": "Those are my books", "Published": True, "Rating": 4, "Id": 2},]
-#region Functions
-
-def find_post_by_id(id: int):
-    for post in my_posts:
-        if post["Id"] == id:
-            return post
-    return None
-#endregion
-
 #region Requests
 @app.get("/")
 def root():
     return {"message": "Hello World this is initial page for the FastAPI."}
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM "tblPosts";""")
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    posts=db.query(models.Post).all()
     return {"Data": posts}
 
 @app.get("/posts/{id}")
-def get_post_by_id(id: int, response: Response):
-    cursor.execute("""SELECT * FROM "tblPosts" WHERE "Id" = %s;""", (str(id)))
-    post=cursor.fetchone()
+def get_post_by_id(id: int, db: Session = Depends(get_db)):
+    post=db.query(models.Post).filter(models.Post.Id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post with id: {id} was not found")
     return {"Data": post}
 
 @app.post("/posts/create", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    cursor.execute("""INSERT INTO "tblPosts" ("Title", "Content", "Published") VALUES (%s, %s, %s) RETURNING * ;""", (post.Title, post.Content, post.Published))
-    new_post = cursor.fetchone()
-    connection.commit()
+def create_post(post: Post, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"Data": new_post}
 
 @app.delete("/posts/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM "tblPosts" WHERE "Id" = %s RETURNING * ;""", (str(id),))
-    deleted_post = cursor.fetchone()
-    connection.commit()
-    if deleted_post == None:
+def delete_post(id: int, db: Session = Depends(get_db)):
+    deleted_post = db.query(models.Post).filter(models.Post.Id == id).delete(synchronize_session=False)
+    db.commit()
+    if not deleted_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post with id: {id} was not found")
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/update/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE "tblPosts" SET "Title" = %s, "Content" = %s, "Published" = %s WHERE "Id" = %s RETURNING * ;""", (post.Title, post.Content, post.Published, str(id)))
-    updated_post = cursor.fetchone()
-    connection.commit()
-    if updated_post == None:
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.Id == id)
+    post = post_query.first()
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post with id: {id} was not found")
-
-    return {"Data": updated_post}
+        
+    post_query.update(updated_post.model_dump(), synchronize_session=False)
+    db.commit()
+    return {"Data": post_query.first()}
 #endregion
---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import TIMESTAMP, Boolean, Column, Integer, String
+from sqlalchemy.sql.expression import text
+from .database import Base 
+
+#region Models
+class Post(Base):
+    __tablename__ = "tblPosts"
+    
+    Id = Column(Integer, primary_key=True, nullable=False)
+    Title = Column(String, nullable=False)
+    Content = Column(String, nullable=False)
+    Published = Column(Boolean, server_default='True', nullable=False)
+    CreatedDate = Column(TIMESTAMP(timezone=True),server_default=text('now()'), nullable=False)
+#endregion
