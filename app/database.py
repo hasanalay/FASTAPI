b4831daa69fa0f31eda7db28cd19ec72fd68d785
hasanalay <hsn_aly@hotmail.com>
2023-08-24 13:51:25 +0300

    switch to the env var instead of hardcoded var

--- app/database.py
+++ app/database.py
@@ -3,9 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from psycopg2.extras import RealDictCursor
 import psycopg2, time
+from .config import settings
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://hasanalay:1973@localhost:5432/fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
